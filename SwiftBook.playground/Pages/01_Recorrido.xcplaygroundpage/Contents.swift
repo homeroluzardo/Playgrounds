aac133d9df1cc98c09845c1fffa806893f13f7ca
Homero Luzardo <homeroeluzardo@gmail.com>
2025-03-15 21:58:47 -0600

    Swift Book

--- SwiftBook.playground/Pages/01_Recorrido.xcplaygroundpage/Contents.swift
+++ SwiftBook.playground/Pages/01_Recorrido.xcplaygroundpage/Contents.swift
@@ -16,3 +16,24 @@ let ancho = 94
 let anchoDeLaEtiqueta = etiqueta + String(ancho) // Al remover el String da error ya que no se puede concatenar un String y un Int.
 print(anchoDeLaEtiqueta)
 
+let manzanas = 3
+let naranjas = 5
+print("Tengo \(manzanas) manzanas")
+print("Tengo \(manzanas + naranjas) frutas")
+
+let numFloat1: Float = 10.5
+let numFloat2: Float = 20.5
+let nombreAlguien: String = "Juan"
+print("Soy \(nombreAlguien) y la suma de los numeros Float es: \(numFloat1 + numFloat2)")
+
+print("")
+
+let cita = """
+     Aun cuando hay espacios en blanco a la izquierda,
+     las líneas no contienen sangría en realidad.
+         Excepto por esta línea.
+     Las comillas dobles (") pueden aparecer sin escaparlas.
+     Todavía tengo \(manzanas + naranjas)
+     frutas.
+     """
+print(cita)
